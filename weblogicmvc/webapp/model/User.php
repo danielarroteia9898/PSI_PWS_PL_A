58d7a5401e29f833bc81d5efd9f3dde45de53c0f
Patricia <2202416@my.ipleiria.pt>
2021-06-19 13:30:14 +0100

    associações

--- weblogicmvc/webapp/model/User.php
+++ weblogicmvc/webapp/model/User.php
@@ -4,5 +4,13 @@ use ActiveRecord\Model;
 
 class User extends Model
 {
+    public static array $has_many = array(
+        array('ticket', ['foreignKey' => 'idutilizador'])
+    );
+
 
 }
+
+$user = User::first();
+
+$ticket = $user->create_ticket(array('precopassagem', 'dtacomprapassagem' => 1));
